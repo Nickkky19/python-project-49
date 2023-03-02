c921e0c38f17bb81444488c748c1e6367313f870
Nickkky19 <nickkky19@mail.ru>
2023-03-02 03:37:44 +0300

    init commit

---
+++ brain_games/scripts/brain_games.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Brain games welcome game."""
+
+
+def main():
+    print('Welcome to the Brain Games!')
+
+
+if __name__ == '__main__':
+    main()
